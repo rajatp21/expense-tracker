158a950046212e8a570902bf22081f4688a8de6b
rajatp21 <95241041+rajatp21@users.noreply.github.com>
2023-07-17 06:09:07 +0530

    day 2

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
-$(document).ready(function() {
-    // Handle form submission
-    $("#expense-form").submit(function(event) {
-      event.preventDefault(); // Prevent form submission
-      
-      // Retrieve expense name and amount
-      const expenseName = $("#expense-name").val();
-      const expenseAmount = parseFloat($("#expense-amount").val());
-  
-      // Validate input
-      if (expenseName === "" || isNaN(expenseAmount)) {
-        alert("Please provide valid inputs.");
-        return;
-      }
-  
-      // Add expense to the list
-      const expenseItem = `<li>${expenseName}: $${expenseAmount.toFixed(2)}</li>`;
-      $("#expenses").append(expenseItem);
-  
-      // Update remaining budget
-      const remainingBudget = parseFloat($("#remaining-budget").text());
-      const newRemainingBudget = remainingBudget - expenseAmount;
-      $("#remaining-budget").text(newRemainingBudget.toFixed(2));
-  
-      // Clear form inputs
-      $("#expense-name").val("");
-      $("#expense-amount").val("");
-    });
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the expense form and expense list elements
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+  // Add event listener to the expense form
+  expenseForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    // Get the form inputs
+    const categoryInput = document.getElementById("category");
+    const amountInput = document.getElementById("amount");
+    const descriptionInput = document.getElementById("description");
+
+    // Create a new expense item element
+    const newExpenseItem = document.createElement("li");
+    newExpenseItem.innerHTML = `
+      <div class="expense-item">
+        <span class="category">${categoryInput.value}</span>
+        <span class="amount">$${amountInput.value}</span>
+        <span class="description">${descriptionInput.value}</span>
+        <button class="edit-btn">Edit</button>
+        <button class="delete-btn">Delete</button>
+      </div>
+    `;
+
+    // Append the new expense item to the expense list
+    expenseList.appendChild(newExpenseItem);
+
+    // Reset the form inputs
+    categoryInput.value = "";
+    amountInput.value = "";
+    descriptionInput.value = "";
   });
-  
+
+  // Add event listener to the expense list
+  expenseList.addEventListener("click", function(e) {
+    if (e.target.classList.contains("edit-btn")) {
+      // Handle edit button click
+      const expenseItem = e.target.closest(".expense-item");
+      const category = expenseItem.querySelector(".category").textContent;
+      const amount = expenseItem.querySelector(".amount").textContent;
+      const description = expenseItem.querySelector(".description").textContent;
+
+      // Perform the desired action for editing an expense
+      console.log("Edit button clicked");
+      console.log("Category:", category);
+      console.log("Amount:", amount);
+      console.log("Description:", description);
+    } else if (e.target.classList.contains("delete-btn")) {
+      // Handle delete button click
+      const expenseItem = e.target.closest("li");
+
+      // Perform the desired action for deleting an expense
+      console.log("Delete button clicked");
+      console.log("Expense item:", expenseItem);
+
+      // Remove the expense item from the DOM
+      expenseItem.remove();
+    }
+  });
+});
